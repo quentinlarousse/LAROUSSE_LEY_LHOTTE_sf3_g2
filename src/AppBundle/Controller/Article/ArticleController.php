adff774e6a3749c20f4961d5303b0d2820c9e42b
quentinlarousse <quentinlazaroto@gmail.com>
2016-03-29 20:02:04 +0200

    push pour thomas
    push pour thomas

--- src/AppBundle/Controller/Article/ArticleController.php
+++ src/AppBundle/Controller/Article/ArticleController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller\Article;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,6 +15,16 @@ class ArticleController extends Controller
     public function listAction()
     {
      return new Response('List of article');
+    }
+
+    /**
+     * @Route("/show/{id}", requirements={"id" = "\d"})
+     */
+    public function showAction($id, Request $request)
+    {
+        dump($request);
+
+        return new Response('Affiche moi l\'article avec l\'id: '.$id);
     }
 
 
