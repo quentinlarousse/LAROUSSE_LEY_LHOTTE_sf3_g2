f7076c211914497bbb7af76721d7d099de0a2324
quentinlarousse <quentinlazaroto@gmail.com>
2016-04-01 14:28:51 +0200

    commit
    commit

---
+++ src/AppBundle/Antispam/Antispam.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Antispam;
+
+class Antispam
+{
+    private $antispamLength;
+
+    public function __construct($antispamLength)
+    {
+        $this->$antispamLength = $antispamLength;
+    }
+    public function isSpam ($text)
+    {
+        return strlen($text) > $this->antispamLength;
+    }
+
+
+}
